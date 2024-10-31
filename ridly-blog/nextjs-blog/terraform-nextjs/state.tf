a4be9e75b458942993b793374ea86cccc1cf11df
ridlock9090 <ridlock@gmail.com>
2024-10-31 02:15:07 -0400

    working file for nodejs

--- ridly-blog/nextjs-blog/terraform-nextjs/state.tf
+++ ridly-blog/nextjs-blog/terraform-nextjs/state.tf
@@ -1,10 +1,10 @@
 
 terraform {
   backend "s3" {
-    bucket = "ridly3-my-terraform-state" 
+    bucket = "ridly4-my-terraform-state" 
     key = "global/s3/terraform.tfstate"    
     region = "us-east-2"
-    dynamodb_table = "terraform-lock-file"
+    dynamodb_table = "terraformjs-lock-file"
     }
         
     
